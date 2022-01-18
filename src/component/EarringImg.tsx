f58c95600e40c8f93b3fe1d7f5c5a9742bea4be0
riko-okubo <12riko14@gmailcom>
2022-01-18 17:01:25 +0900

    不要ファイル消去

--- src/component/EarringImg.tsx
+++ src/component/EarringImg.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 
-type Item= {
-    img: string;
-    title: string;
-}
-// export default function EarringImg() 
+import hook_Blue from './Image/hook_Blue.jpg';
+import hook_BlueSkyblueWhite from './Image/hook_BlueSkyblueWhite.jpg';
+import hook_BlueWhite from './Image/hook_BlueWhite.jpg';
+import hook_GreenWhite from './Image/hook_GreenWhite.jpg';
+import hook_OrangeYellowWhite from './Image/hook_OrangeYellowWhite.jpg';
+import hook_PinkRed from './Image/hook_PinkRed.jpg';
+import hook_Red from './Image/hook_Red.jpg';
+import hook_RedOrangeYellow from './Image/hook_RedOrangeYellow.jpg';
+import hook_VioletRedPink from './Image/hook_VioletRedPink.jpg';
+import stud_Blue from './Image/stud_Blue.jpg';
+import stud_BlueWhite from './Image/stud_BlueWhite.jpg';
+import stud_Green from './Image/stud_Green.jpg';
+import stud_Orange from './Image/stud_Orange.jpg';
+import stud_OrangeGreenYellow from './Image/stud_OrangeGreenYellow.jpg';
+import stud_RedWhite from './Image/stud_RedWhite.jpg';
+import stud_VioletPinkWhite from './Image/stud_VioletPinkWhite.jpg';
+
 export const  EarringImg = () => {
+
     return (
-        <Box sx={{ width: 500, height: 450, overflowY: 'scroll' }}>
+        <Box sx={{ width: '80%', height: 1, overflowY: 'scroll',  margin: 'auto'}}>
             <ImageList variant="masonry" cols={3} gap={8}>
                 {itemData.map((item) => (
                     <ImageListItem key={item.img}>
@@ -27,58 +41,70 @@ export const  EarringImg = () => {
     );
 }
 
-//デモのデータではなく、自分の写真データをインポートして作成する必要あり。
-//ImageListのファイル?をつくる。
-//型に詰まる可能性大
-
 const itemData = [
     {
-      img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
-      title: 'Bed',
+      img: hook_Blue,
+      title: 'hook_blue',
     },
     {
-      img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
-      title: 'Books',
+      img: hook_BlueSkyblueWhite,
+      title: 'hook_blue-skyblue-white',
     },
     {
-      img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
-      title: 'Sink',
+      img: hook_BlueWhite,
+      title: 'hook_blue-white',
     },
     {
-      img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
-      title: 'Kitchen',
+      img: hook_GreenWhite,
+      title: 'hook_green-white',
     },
     {
-      img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
-      title: 'Blinds',
+      img: hook_OrangeYellowWhite,
+      title: 'hook_orange-yellow-white',
     },
     {
-      img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
-      title: 'Chairs',
+      img: hook_PinkRed,
+      title: 'hook_PinkRed',
     },
     {
-      img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
-      title: 'Laptop',
+      img: hook_Red,
+      title: 'hook_Red',
     },
     {
-      img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
-      title: 'Doors',
+      img: hook_RedOrangeYellow,
+      title: 'hook_RedOrangeYellow',
     },
     {
-      img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
-      title: 'Coffee',
+      img: hook_VioletRedPink,
+      title: 'hook_VioletRedPink',
     },
     {
-      img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
-      title: 'Storage',
+      img: stud_Blue,
+      title: 'stud_Blue',
     },
     {
-      img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
-      title: 'Candle',
+      img: stud_BlueWhite,
+      title: 'stud_BlueWhite',
     },
     {
-      img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
-      title: 'Coffee table',
+      img: stud_Green,
+      title: 'stud_Green',
+    },
+    {
+      img: stud_Orange,
+      title: 'stud_Orange',
+    },
+    {
+      img: stud_OrangeGreenYellow,
+      title: 'stud_OrangeGreenYellow',
+    },
+    {
+      img: stud_RedWhite,
+      title: 'stud_RedWhite',
+    },
+    {
+      img: stud_VioletPinkWhite,
+      title: 'stud_VioletPinkWhite',
     },
   ];
   
