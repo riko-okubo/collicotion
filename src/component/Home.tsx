764e168e8920b0ecf75c18e1e6334162b5461eac
riko-okubo <12riko14@gmailcom>
2022-01-22 22:57:52 +0900

    背景とtitle

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -7,14 +7,32 @@ import { green } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
+        head: {
+            position: 'relative',
+            margin: 0
+        },
         background: {
-            backgroundImage: `url(${backgroundImg})`,
-            backgroundSize: 'contain',
-            backgroundRepeat: 'no-repeat',
-            height:'100vh'
+            width: '100%',
+            margin: 0
         },
         title: {
-            margin: 'auto'
+            textAlign: 'center',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%,-50%)'
+        },
+        main_title: {
+            fontFamily: 'cursive',
+            color: '#F2EADF',
+            fontSize: '3em',
+            margin: 0
+        },
+        sub_title: {
+            fontFamily: 'cursive',
+            color: '#F2EADF',
+            fontSize: '2em',
+            margin:0
         }
     })
 );
@@ -22,8 +40,12 @@ const useStyles = makeStyles((theme: Theme) =>
 export const Home = () => {
     const classes = useStyles();
     return(
-        <div className={classes.background}>
-            <h1 className={classes.title}>coilliction</h1>
+        <div className={classes.head}>
+            <img className={classes.background} src={backgroundImg} />
+            <div className={classes.title}>
+                <h1 className={classes.main_title}>coilliction</h1>
+                <p className={classes.sub_title}>~ My handmade collection ~</p>
+            </div>
             {/* <div><Link to="/EarringImg_Masonry">EarringImg_Masonry</Link></div> */}
             {/* <div><Link to="/EarringImg_Quilted">EarringImg_Quilted</Link></div> */}
         </div>
