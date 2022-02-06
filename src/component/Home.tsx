0d82cd509b49c80685c9bd134aaf7928769fedf9
riko-okubo <12riko14@gmailcom>
2022-02-06 15:13:15 +0900

    TopPage レイアウト完成

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -2,21 +2,53 @@ import React from 'react';
 import { useStyles } from './HomeStyle';
 
 import backgroundImg from '../component/Image/backgroundImg.jpg';
+import EarringsGroup from '../component/Image/EarringsGroup.jpg';
+import FlowersGroup from '../component/Image/FlowersGroup.jpg';
 
 export const Home = () => {
     const classes = useStyles();
 
     return (
         <>
-            <div className={classes.top}>
+            {/* <div className={classes.header}>
+
+            </div> */}
+            
+        <div className={classes.top}>
                 <img className={classes.top_background} src={backgroundImg} />
                 <div className={classes.curtain}>
                     <div className={classes.title}>
-                        <h1 className={classes.main_title}>coillicotion</h1>
-                        <p className={classes.sub_title}>~ My handmade collection ~</p>
+                        <h1>coillicotion</h1>
+                        <p>~ My handmade collection ~</p>
+                    </div>
+                </div>
+            
+
+            <div className={classes.body}>
+                <div className={classes.content}>
+                    <img src={EarringsGroup} />
+                    <div className={classes.sub_title}>
+                        <h3>Earrings</h3>
+                        <p>小さなドライフラワーをUVレジンで包んだピイアスとイヤリング</p>
+                    </div>
+                </div>
+                <div className={classes.content}>
+                    <img src={FlowersGroup} />
+                    <div className={classes.sub_title}>
+                        <h3>3D Flowers</h3>
+                        <p>ワイヤーで成形し、マニキュアとUVレジンで膜を張った花びらで作ったお花</p>
                     </div>
                 </div>
             </div>
+
+            <div className={classes.footer}>
+                <ul>
+                    <li>アクセサリーについて</li>
+                    <li>ホームページ製作記録</li>
+                    <li>プロフィール</li>
+                </ul>
+            </div>
+        </div>
         </>
     )
 }
--- src/component/HomeStyle.tsx
+++ src/component/HomeStyle.tsx
@@ -28,16 +28,61 @@ export const useStyles = makeStyles((theme:Theme) =>
             textAlign: 'center',
             position: 'absolute',
             top: '50%',
-            left: '50%',
-            transform: 'translate(-50%,-50%)'
+            justifyContent: 'center',
+            "& h1": {
+                fontSize: '3em',
+                margin: 0
+            },
+            "& p": {
+                fontSize: '1.8em',
+                margin: 0
+            }
         },
-        main_title: {
-            fontSize: '3em',
-            margin: 0
+
+        body: {
+            fontFamily: 'cursive',
+            color: '#666A71',
+            margin: '6em auto'
+        },
+        content: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            "& img": {
+                margin: '3em',
+                width: '25em',
+                height: 'auto'
+            },
         },
         sub_title: {
-            fontSize: '1.8em',
-            margin: 0
+            fontFamily: 'cursive',
+            color: '#666A71',
+            textAlign: 'center',
+            width: '30em',
+            "& h3": {
+                fontSize: '3em'
+            },
+            "& p": {
+                fontSize: '1em'
+            }
         },
+
+        footer: {
+            backgroundColor: '#F2EADF',
+            display: 'inline-block',
+            width: '100%',
+            bottom: 0,
+            "& ul": {
+                margin: '2.5em 1.5em',
+                textAlign: 'end',
+                listStyleType: 'none'
+            },
+            "& li":{
+                margin: '1em',
+                fontFamily: 'cuesive',
+                color: '#666A71',
+                fontSize: '1em'
+            }
+        }
     })
 )
