09efa27b6db8db861b1da925c1f2e5b98c957566
riko-okubo <12riko14@gmailcom>
2022-03-08 22:22:37 +0900

    Home responsive

--- src/component/HomeStyle.tsx
+++ src/component/HomeStyle.tsx
@@ -1,5 +1,4 @@
 import { createStyles, makeStyles,Theme } from "@material-ui/core/styles";
-import { theme } from "./style/theme";
 
 export const useStyles = makeStyles((theme:Theme) => 
     createStyles({
