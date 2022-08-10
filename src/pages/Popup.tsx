5a24b1bd3301e71da7eb9f5451af5d7cec9063d0
riko <81846197+riko-okubo@users.noreply.github.com>
2022-08-10 16:40:09 +0900

    zoomin animatin

--- src/pages/Popup.tsx
+++ src/pages/Popup.tsx
@@ -2,6 +2,7 @@ import { Button, Modal } from "@material-ui/core";
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useStyles } from "../style/HomeStyle";
+import { useSpring, animated } from "react-spring";
 import emerald_rose from "../component/atoms/Image/emerald_rose.jpg";
 
 type Props = {
@@ -10,12 +11,19 @@ type Props = {
 };
 
 export const Popup = (props: Props) => {
+  const [zoomin, setZoomin] = useSpring(() => ({
+    from: { scale: 0 },
+  }));
   const classes = useStyles();
   const navigate = useNavigate();
 
+  useEffect(() => {
+    setZoomin({ scale: 0.9, config: { friction: 60 } });
+  }, []);
+
   return (
     <Modal open={props.popupOpen}>
-      <div className={classes.popup}>
+      <animated.div className={classes.popup} style={zoomin}>
         <div className={classes.closeButton}>
           <Button onClick={() => props.setPopupOpen(false)}>✕</Button>
         </div>
@@ -26,7 +34,7 @@ export const Popup = (props: Props) => {
         <div className={classes.openPageButton}>
           <Button onClick={() => navigate("/Flowers")}>open↗</Button>
         </div>
-      </div>
+      </animated.div>
     </Modal>
   );
 };
